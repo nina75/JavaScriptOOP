ddf4ce2c9646ea94cca03e37606ff6213b2578f4
nina75 <kozasbrada28>
2016-02-01 21:11:11 +0200

    DOM traversal

--- SoftUni/Homework_FunctionsAndFunctionExpressions/Problem_06_DOMTraversal.js
+++ SoftUni/Homework_FunctionsAndFunctionExpressions/Problem_06_DOMTraversal.js
@@ -2,3 +2,31 @@
    <Element name>: id="<id>", class="<class>"
    Print each element on a new line. Indent child elements. */
 
+function traverse(selector) {
+    var element = document.querySelector(selector);
+    function printChildren(el, symbol) {
+        var ids, classes, index, child;
+        symbol = symbol || ' ';
+        var children = el.childNodes;
+        for(index in children) {
+            classes = '';
+            ids = '';
+            child = children[index];
+            if(child.nodeType == 1) {
+                if(child.id) {
+                    ids += ' id="' + child.id + '"';
+                }
+                if(child.classList.length) {
+                    classes += ' class="' + child.className + '" ';
+                }
+                console.log(symbol + child.nodeName.toLowerCase() + ':' + ids + classes);
+            }
+            printChildren(child, symbol + '    ');
+        }
+    }
+    printChildren(element);
+}
+
+var selector = ".birds";
+traverse(selector);
+
