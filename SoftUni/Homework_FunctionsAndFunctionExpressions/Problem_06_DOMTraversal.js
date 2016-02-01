22bb50e0bdc4e8b78cf31c346d68ed0f78ee6511
nina75 <kozasbrada28>
2016-02-01 19:19:11 +0200

    Function composition

---
+++ SoftUni/Homework_FunctionsAndFunctionExpressions/Problem_06_DOMTraversal.js
@@ -0,0 +1,4 @@
+/* You are given an HTML file index.html. Write a function that traverses all child elements of an element by a given CSS selector and prints all found elements in the format:
+   <Element name>: id="<id>", class="<class>"
+   Print each element on a new line. Indent child elements. */
+
